b91d033f81fa842ee16b777ed30d72be6747f4ac
David Case <david.case@shruggr.cloud>
2023-07-04 21:18:31 +0000

    Add B parsing

--- dist/lib.js
+++ dist/lib.js
@@ -6,33 +6,35 @@ const bitcore_lib_1 = require("bitcore-lib");
 const dns = require("dns/promises");
 const http_errors_1 = require("http-errors");
 const provider_1 = require("./provider");
+const B = Buffer.from('19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut');
+const ORD = Buffer.from('ord');
 let btcProvider = new provider_1.BtcProvider();
 let bsvProvider = new provider_1.JungleBusProvider();
 if (process.env.BITCOIN_HOST) {
-    bsvProvider = new provider_1.RpcProvider('bsv', process.env.BITCOIN_HOST || '', process.env.BITCOIN_PORT || '8332', process.env.BITCOIN_USER || '', process.env.BITCOIN_PASS || '');
+    bsvProvider = new provider_1.RpcProvider("bsv", process.env.BITCOIN_HOST || "", process.env.BITCOIN_PORT || "8332", process.env.BITCOIN_USER || "", process.env.BITCOIN_PASS || "");
 }
 if (process.env.BTC_HOST) {
-    btcProvider = new provider_1.RpcProvider('btc', process.env.BTC_HOST || '', process.env.BTC_PORT || '8332', process.env.BTC_USER || '', process.env.BTC_PASS || '');
+    btcProvider = new provider_1.RpcProvider("btc", process.env.BTC_HOST || "", process.env.BTC_PORT || "8332", process.env.BTC_USER || "", process.env.BTC_PASS || "");
 }
 async function getLatestBlock(network) {
     switch (network) {
-        case 'btc':
+        case "btc":
             return btcProvider.getBlockchainInfo();
-        case 'bsv':
+        case "bsv":
             return bsvProvider.getBlockchainInfo();
         default:
-            throw new http_errors_1.NotFound('Network Not Found');
+            throw new http_errors_1.NotFound("Network Not Found");
     }
 }
 exports.getLatestBlock = getLatestBlock;
 async function getRawTx(network, txid) {
     switch (network) {
-        case 'btc':
+        case "btc":
             return btcProvider.getRawTx(txid);
-        case 'bsv':
+        case "bsv":
             return bsvProvider.getRawTx(txid);
         default:
-            throw new http_errors_1.NotFound('Network Not Found');
+            throw new http_errors_1.NotFound("Network Not Found");
     }
 }
 exports.getRawTx = getRawTx;
@@ -62,21 +64,21 @@ async function loadInscription(pointer) {
     console.log("loadInscription", pointer);
     let script;
     if (pointer.match(/^[0-9a-fA-F]{64}_\d*$/)) {
-        const [txid, vout] = pointer.split('_');
-        console.log('BSV:', txid, vout);
+        const [txid, vout] = pointer.split("_");
+        console.log("BSV:", txid, vout);
         const rawtx = await bsvProvider.getRawTx(txid);
         const tx = core_1.Tx.fromBuffer(rawtx);
         script = tx.txOuts[parseInt(vout, 10)].script;
     }
     else if (pointer.match(/^[0-9a-fA-F]{64}i\d+$/) && btcProvider) {
-        const [txid, vin] = pointer.split('i');
-        console.log('BTC', txid, vin);
+        const [txid, vin] = pointer.split("i");
+        console.log("BTC", txid, vin);
         const rawtx = await btcProvider.getRawTx(txid);
         const tx = new bitcore_lib_1.Transaction(rawtx);
         script = core_1.Script.fromBuffer(tx.inputs[parseInt(vin, 10)].witnesses[1]);
     }
     else
-        throw new Error('Invalid Pointer');
+        throw new Error("Invalid Pointer");
     if (!script)
         throw new http_errors_1.NotFound();
     const file = parseScript(script);
@@ -86,35 +88,34 @@ async function loadInscription(pointer) {
 }
 exports.loadInscription = loadInscription;
 function parseScript(script) {
-    var _a, _b, _c;
+    var _a, _b, _c, _d;
     let opFalse = 0;
     let opIf = 0;
     let opORD = 0;
-    const lock = new core_1.Script();
+    let type = "application/octet-stream";
+    let data = Buffer.alloc(0);
     for (let [i, chunk] of script.chunks.entries()) {
+        if (((_a = chunk.buf) === null || _a === void 0 ? void 0 : _a.equals(B)) && script.chunks.length > i + 2) {
+            data = script.chunks[i + 1].buf;
+            type = script.chunks[i + 2].buf.toString();
+            return { data, type };
+        }
         if (chunk.opCodeNum === core_1.OpCode.OP_FALSE) {
             opFalse = i;
         }
         if (chunk.opCodeNum === core_1.OpCode.OP_IF) {
             opIf = i;
         }
-        if ((_a = chunk.buf) === null || _a === void 0 ? void 0 : _a.equals(Buffer.from('ord', 'utf8'))) {
-            if (opFalse === i - 2 && opIf === i - 1) {
-                opORD = i;
-                lock.chunks = script.chunks.slice(0, i - 2);
-                break;
-            }
+        if (((_b = chunk.buf) === null || _b === void 0 ? void 0 : _b.equals(ORD)) && opFalse === i - 2 && opIf === i - 1) {
+            opORD = i;
+            break;
         }
-        lock.chunks.push(chunk);
     }
-    let type = 'application/octet-stream';
-    let data = Buffer.alloc(0);
     for (let i = opORD + 1; i < script.chunks.length; i++) {
-        // console.log(script.chunks[i])
         switch (script.chunks[i].opCodeNum) {
             case core_1.OpCode.OP_FALSE:
-                while (((_b = script.chunks[i + 1]) === null || _b === void 0 ? void 0 : _b.opCodeNum) >= 1 &&
-                    ((_c = script.chunks[i + 1]) === null || _c === void 0 ? void 0 : _c.opCodeNum) <= core_1.OpCode.OP_PUSHDATA4) {
+                while (((_c = script.chunks[i + 1]) === null || _c === void 0 ? void 0 : _c.opCodeNum) >= 1 &&
+                    ((_d = script.chunks[i + 1]) === null || _d === void 0 ? void 0 : _d.opCodeNum) <= core_1.OpCode.OP_PUSHDATA4) {
                     data = Buffer.concat([data, script.chunks[i + 1].buf]);
                     i++;
                 }
@@ -124,7 +125,7 @@ function parseScript(script) {
                 if (script.chunks[i].buf[0] != 1)
                     return;
             case core_1.OpCode.OP_TRUE:
-                type = script.chunks[i + 1].buf.toString('utf8');
+                type = script.chunks[i + 1].buf.toString("utf8");
                 // console.log("Type:", type)
                 i++;
                 break;
